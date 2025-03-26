9510af962eca10a8f47e9a7506c2a8e6cb2089fe
Christian <150170670+ThirdRevelation7@users.noreply.github.com>
2025-03-26 12:46:33 -0400

    Change mobile effects in About section

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -30,16 +30,28 @@ export const About = () => {
                     </h2>
 
                     <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all duration-300 hover:shadow-[0_0_15px_theme('color.pink.500')]"
-                        onTouchStart={(e) =>
+                        onTouchStart={(e) => {
                             e.currentTarget.classList.add(
                                 "shadow-[0_0_15px_theme('color.pink.500')]"
-                            )
-                        }
-                        onTouchEnd={(e) =>
+                            );
+                            e.currentTarget.querySelectorAll(".pink-glow").forEach((span) => {
+                                span.classList.add("touch-hover-pink");
+                            });
+                            e.currentTarget.querySelectorAll(".cyan-glow").forEach((span) => {
+                                span.classList.add("touch-hover-cyan");
+                            });
+                        }}
+                        onTouchEnd={(e) => {
                             e.currentTarget.classList.remove(
                                 "shadow-[0_0_15px_theme('color.pink.500')]"
-                            )
-                        }
+                            );
+                            e.currentTarget.querySelectorAll(".pink-glow").forEach((span) => {
+                                span.classList.remove("touch-hover-pink");
+                            });
+                            e.currentTarget.querySelectorAll(".cyan-glow").forEach((span) => {
+                                span.classList.remove("touch-hover-cyan");
+                            });
+                        }}
                     >
                         <p className="text-gray-400 mb-6 ">
                             What I may lack in work experience, I will more than make up for with drive, hard work, and focus. I thrive in team environments and will be extremely grateful for any opportunity to contribute and grow.
@@ -52,9 +64,7 @@ export const About = () => {
                                         <span
                                             key={key}
                                             className="bg-cyan-200/10 hover:bg-cyan-200/20 rounded-full text-gray-200
-                                                   py-1 px-3 text-sm transition hover:shadow-[0_0_15px_theme('colors.cyan.200')] hover:text-cyan-200"
-                                            onTouchStart={(e) => e.currentTarget.classList.add("touch-hover-cyan")}
-                                            onTouchEnd={(e) => e.currentTarget.classList.remove("touch-hover-cyan")}
+                                                   py-1 px-3 text-sm transition hover:shadow-[0_0_15px_theme('colors.cyan.200')] hover:text-cyan-200 cyan-glow"
                                         >
                                             {tech}
 
@@ -69,9 +79,7 @@ export const About = () => {
                                         <span
                                             key={key}
                                             className="bg-pink-500/10 hover:bg-pink-500/20 rounded-full text-gray-200
-                                                   py-1 px-3 text-sm transition hover:shadow-[0_0_15px_theme('colors.pink.500')] hover:text-pink-500"
-                                            onTouchStart={(e) => e.currentTarget.classList.add("touch-hover-pink")}
-                                            onTouchEnd={(e) => e.currentTarget.classList.remove("touch-hover-pink")}
+                                                   py-1 px-3 text-sm transition hover:shadow-[0_0_15px_theme('colors.pink.500')] hover:text-pink-500 pink-glow"
                                         >
                                             {tech}
 
